757da82779501e0c3fe0847b85670568b405aa04
netlight <jasoness@yandex.ru>
2019-12-28 21:39:34 +0300

    - Fix code problems - Add death - Add treasure as actor - Add defend - Not add inventory (sadly)

--- src/com/netlighter/Actor.java
+++ src/com/netlighter/Actor.java
@@ -3,15 +3,20 @@ package com.netlighter;
 import java.util.Random;
 
 public class Actor {
-    static class Enemy{
 
+    static class Enemy{
         Random r = new Random();
         int health;
         int level;
+        int loot_experience;
+        int loot_money;
 
         Enemy(int l){
-            level = l+r.nextInt(3);
-            health = 100*level;
+            level = l+r.nextInt(3)/2;
+            health = 20*level;
+            loot_experience = r.nextInt((health - health/2)+1) + health/2;
+            loot_money = r.nextInt((level*10 - level*5)+1) + level*5;
+
         }
         Char attack(Char ch){
             if(ch.armor != 0) {
@@ -23,6 +28,15 @@ public class Actor {
             return ch;
         }
     }
-    interface Treasure{}
+    static class Treasure{
+        Random r = new Random();
+        int loot_experience;
+        int loot_money;
+
+        Treasure(int level){
+            loot_experience = r.nextInt((level*70 - level*30)+1) + level*30;
+            loot_money = r.nextInt((level*10 - level*5)+1) + level*5;
+        }
+    }
 
 }
--- src/com/netlighter/Event.java
+++ src/com/netlighter/Event.java
@@ -19,17 +19,29 @@ public class Event {
 
         }
     }
+// TODO MAKE 'EM INHER ACTOR OR ANY OPTIMIZE
+    static void loot(Char ch, Actor.Treasure tr){
+        ch.experience += tr.loot_experience;
+        ch.money += tr.loot_money;
+        System.out.println(
+                tr.loot_experience + " exp gained!\n" +
+                        tr.loot_experience + " money gained!\n");
+    }
+
+    static void loot(Char ch, Actor.Enemy en){
+        ch.experience +=en.loot_experience;
+        ch.money += en.loot_money;
+        System.out.println(
+                en.loot_experience + " exp gained!\n" +
+                        en.loot_experience + " money gained!\n");
+    }
+
 
     static void findTreasure(Char ch) {
-        double expGain = r.nextInt(100);
-        int moneyGain = r.nextInt(10);
-        ch.experience += expGain;
-        ch.money += moneyGain;
-        System.out.println(
-                "Nice! You found treasure!\n" +
-                        expGain + " exp gained!\n" +
-                        moneyGain + " money gained!\n"
-        );
+        Actor.Treasure tr = new Actor.Treasure(ch.level);
+        System.out.println("Nice! You found treasure!\n");
+        loot(ch, tr);
+
     }
 
     static void doNothing() {
@@ -38,15 +50,38 @@ public class Event {
     }
 
     static void findEnemy(GameController gm, Char ch) {
+        String dec;
         Actor.Enemy en = new Actor.Enemy(ch.level);
+
         System.out.println("You found enemy!!!");
-        while (en.health > 0) {
-            String dec = gm.makeDecision("What?", "Attack!!!", "Defend!!!");
-            if (dec == "Attack!!!") {
+
+        while (en.health > 0 && ch.health > 0) {
+            dec = gm.makeDecision("What?", "Attack!!!", "Defend!!!");
+
+            if (dec == "Attack!!!"){
                 en = ch.makeDamage(en);
+                en.attack(ch);
             }
-            en.attack(ch);
+            else if (dec == "Defend!!!") ch.health -= 1;
+
             System.out.println(ch.health + " health\n" + en.health + " enemy health\n");
+        }
+        if (ch.health <= 0) death(gm, ch);
+        if (en.health <=0) loot(ch, en);
+
+    }
+
+    static void death(GameController gm, Char ch){
+        String dec;
+        dec = gm.makeDecision("YOU DIED",
+                "Use Health Potion!(Infinity)", "Rest in peace..."); // TODO ADD HEALTH POTION PLEASE
+        if (dec == "Use Health Potion!(Infinity)") {
+            ch.health = 100;
+        } // TODO SAME
+        else if (dec == "Rest in peace..."){
+            System.out.println("Well...");
+            ch.checkParams();
+            gm.gameEnd();
         }
     }
 }
